50b83d0a55a32afae7d940ff0e004e3823144fbf
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-04-10 17:52:51 +0700

    [locales] move language value management to SettingsHelper

--- app/src/main/java/me/capcom/smsgateway/helpers/LocaleHelper.kt
+++ app/src/main/java/me/capcom/smsgateway/helpers/LocaleHelper.kt
@@ -3,35 +3,18 @@ package me.capcom.smsgateway.helpers
 import android.content.Context
 import android.content.res.Configuration
 import android.content.res.Resources
-import androidx.preference.PreferenceManager
 import java.util.Locale
 
 object LocaleHelper {
-    private const val SELECTED_LANGUAGE = "app.language"
-
     fun onAttach(context: Context): Context {
-        val lang = getPersistedData(context, "")
+        val lang = SettingsHelper(context).language
         return updateResources(context, lang)
     }
 
-    fun getLanguage(context: Context): String {
-        return getPersistedData(context, Locale.getDefault().language)
-    }
-
     fun setLocale(context: Context, language: String): Context {
-        persist(context, language)
+        SettingsHelper(context).language = language
 
         return updateResources(context, language)
-    }
-
-    private fun getPersistedData(context: Context, defaultLanguage: String): String {
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        return preferences.getString(SELECTED_LANGUAGE, defaultLanguage) ?: defaultLanguage
-    }
-
-    private fun persist(context: Context, language: String) {
-        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-        preferences.edit().putString(SELECTED_LANGUAGE, language).apply()
     }
 
     private fun updateResources(context: Context, language: String): Context {
